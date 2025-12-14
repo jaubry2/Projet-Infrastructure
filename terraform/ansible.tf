23ead9bfae2746357329be0f2dc4bb0d5e6df34d
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-14 16:24:53 +0100

    refactor : on met toutes les infos de conn sur la même ligne

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -2,16 +2,12 @@ resource "local_file" "ansible_inventory" {
   filename = "../inventory.ini"
   content  = <<-EOT
 [master]
-${google_compute_instance.nodes["master"].name} ansible_host=${google_compute_instance.nodes["master"].network_interface[0].access_config[0].nat_ip}
+${google_compute_instance.nodes["master"].name} ansible_host=${google_compute_instance.nodes["master"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
 
 [workers]
-${google_compute_instance.nodes["worker"].name} ansible_host=${google_compute_instance.nodes["worker"].network_interface[0].access_config[0].nat_ip}
+${google_compute_instance.nodes["worker"].name} ansible_host=${google_compute_instance.nodes["worker"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
 
 [edge]
-${google_compute_instance.nodes["edge"].name} ansible_host=${google_compute_instance.nodes["edge"].network_interface[0].access_config[0].nat_ip}
-
-[all:vars]
-ansible_user=${var.ssh_user}
-ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
+${google_compute_instance.nodes["edge"].name} ansible_host=${google_compute_instance.nodes["edge"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
 EOT
 }
