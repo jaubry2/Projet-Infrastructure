f53c5aea5bd8085d4c114ac6611e30b329a01b62
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-14 17:31:42 +0100

    feat : création du fichier inventiry.ini après le deploy de toutes les vms

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,13 +1,27 @@
 resource "local_file" "ansible_inventory" {
-  filename = "../inventory.ini"
+  filename = "${path.module}/../inventory.ini"
   content  = <<-EOT
+[all:vars]
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+namenode_ip=${google_compute_instance.nodes["master"].network_interface[0].access_config[0].nat_ip}
+
 [master]
 ${google_compute_instance.nodes["master"].name} ansible_host=${google_compute_instance.nodes["master"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
 
-[workers]
-${google_compute_instance.nodes["worker"].name} ansible_host=${google_compute_instance.nodes["worker"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
-
 [edge]
 ${google_compute_instance.nodes["edge"].name} ansible_host=${google_compute_instance.nodes["edge"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}
+
+[workers]
+%{ for key, node in google_compute_instance.nodes ~}
+%{ if startswith(key, "worker") }
+${node.name} ansible_host=${
+    # LOGIQUE DE SÉLECTION DE L'IP :
+    # Si la longueur de access_config est > 0 (il y a une IP publique), on utilise nat_ip.
+    # Sinon (IP publique absente), on utilise network_ip (IP Privée).
+    length(node.network_interface[0].access_config) > 0 ? "${node.network_interface[0].access_config[0].nat_ip} ansible_host=${google_compute_instance.nodes["edge"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}" : "${node.network_interface[0].network_ip} ansible_ssh_common_args='-o ProxyJump=terraform-user@${google_compute_instance.nodes["master"].network_interface[0].access_config[0].nat_ip}' ansible_host=${google_compute_instance.nodes["edge"].network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${replace(var.ssh_pub_key, ".pub", "")}"
+}
+%{ endif ~}
+%{ endfor ~}
+
 EOT
 }
