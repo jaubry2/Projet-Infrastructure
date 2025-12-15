3c7b35a08c906cc2a7ead9737848fcbe268cf037
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-15 16:24:32 +0100

    doc : utiles

--- commande.sh
+++ commande.sh
@@ -1,3 +1,7 @@
 ansible-playbook -i inventory.ini ./ansible/master.yml
-ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.76.173.135
-ssh -i ~/.ssh/gcp_vm_key_nopass -o ProxyCommand="ssh -W %h:%p -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.76.173.135" terraform-user@10.10.0.5
+ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.78.131.236
+ssh -i ~/.ssh/gcp_vm_key_nopass -o ProxyCommand="ssh -W %h:%p -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.78.131.236" terraform-user@10.10.0.5
+ssh-keygen -R 10.10.0.5
+
+ssh -i /home/terraform-user/.ssh/gcp_vm_key_nopass terraform-user@34.77.148.49
+
