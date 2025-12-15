24b608bf918755cb8fc559546e167f4b8d85ef3d
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-15 13:36:14 +0100

    doc : commande utile

--- commande.sh
+++ commande.sh
@@ -1,2 +1,3 @@
 ansible-playbook -i inventory.ini ./ansible/master.yml
-ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.76.161.131
+ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.76.173.135
+ssh -i ~/.ssh/gcp_vm_key_nopass -o ProxyCommand="ssh -W %h:%p -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.76.173.135" terraform-user@10.10.0.5
