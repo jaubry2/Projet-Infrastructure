44ddac6221145c6221ec7210eacc4205abda73ff
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-15 12:25:55 +0100

    feat : création d'un router nat pour faire communiquer à l'extérieur les worker sans adresse publique

--- terraform/network.tf
+++ terraform/network.tf
@@ -25,3 +25,36 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags = ["http-server", "ansible-target", "ssh-access"]
   description = "Autorise le trafic SSH entrant pour la connexion Ansible."
 }
+
+# Routeur Cloud pour le NAT
+resource "google_compute_router" "nat_router" {
+  name    = "router-for-nat"
+  network = google_compute_network.custom_vpc_network.name
+  region  = google_compute_subnetwork.custom_vpc_subnet.region
+}
+
+# Configuration du service NAT sur le routeur
+resource "google_compute_router_nat" "cloud_nat" {
+  name                               = "nat-config"
+  router                             = google_compute_router.nat_router.name
+  region                             = google_compute_router.nat_router.region
+  
+  # Achemine le trafic de TOUTES les IPs privées (non-publiques)
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  
+  # Utilise des IPs NAT automatiques fournies par Google
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  
+  # Définit la plage de sous-réseaux à surveiller pour le NAT
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+  
+  # Indique que le NAT s'applique au sous-réseau spécifique
+  subnetwork {
+    name                 = google_compute_subnetwork.custom_vpc_subnet.self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+}
