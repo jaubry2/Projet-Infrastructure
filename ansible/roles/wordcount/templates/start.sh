ec8ffd6f62c1c69304b161739c7e68fbd0c4ee1d
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-17 16:41:05 +0100

    doc : fichier de config pour executer le wordcount

--- ansible/roles/wordcount/templates/start.sh
+++ ansible/roles/wordcount/templates/start.sh
@@ -1,7 +1,7 @@
 
 rm -rf /tmp/hadoop*
-ssh -i /home/terraform-user/.ssh/gcp_vm_key_nopass terraform-user@{{datanodes_ips.split(",")[0]}} rm -rf /tmp/hadoop*
-ssh -i /home/terraform-user/.ssh/gcp_vm_key_nopass terraform-user@{{datanodes_ips.split(",")[1]}} rm -rf /tmp/hadoop*
+ssh {{datanodes_ips.split(",")[0]}} rm -rf /tmp/hadoop*
+ssh {{datanodes_ips.split(",")[1]}} rm -rf /tmp/hadoop*
 
 hdfs namenode -format
 
