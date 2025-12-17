69de75bc989c9441aa61d0bc8bfaf3e136b97b38
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-17 18:16:04 +0100

    doc : utile

--- commande.sh
+++ commande.sh
@@ -1,7 +1,12 @@
 ansible-playbook -i inventory.ini ./ansible/master.yml
-ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.78.131.236
-ssh -i ~/.ssh/gcp_vm_key_nopass -o ProxyCommand="ssh -W %h:%p -i ~/.ssh/gcp_vm_key_nopass terraform-user@34.78.131.236" terraform-user@10.10.0.5
+ssh -i ~/.ssh/gcp_vm_key_nopass terraform-user@35.195.241.125
+ssh -i ~/.ssh/gcp_vm_key_nopass -o ProxyCommand="ssh -W %h:%p -i ~/.ssh/gcp_vm_key_nopass terraform-user@35.195.241.125" terraform-user@10.10.0.5
 ssh-keygen -R 10.10.0.5
 
 ssh -i /home/terraform-user/.ssh/gcp_vm_key_nopass terraform-user@34.77.148.49
 
+# Pour lire TOUS les fichiers du répertoire à la suite
+hdfs dfs -cat /output/part*
+
+# Ou fusionne tous les fichiers de sortie en un seul fichier local
+hdfs dfs -getmerge /output resultat_final.txt
