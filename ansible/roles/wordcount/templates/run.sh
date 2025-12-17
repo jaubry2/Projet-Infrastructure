0273ab9ef1650a0070796c722e8c3cad9e896596
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-17 18:16:54 +0100

    feat : on ajoute l'ip du master au script

--- ansible/roles/wordcount/templates/run.sh
+++ ansible/roles/wordcount/templates/run.sh
@@ -1,4 +1,4 @@
 
 hdfs dfs -rm /output/*
 hdfs dfs -rmdir /output
-spark-submit --class WordCount --master spark://master:7077 wc.jar hdfs://master:9000/input hdfs://master:9000/output
+spark-submit --class WordCount --master spark://{{namenode_ip}}:7077 wc.jar hdfs://{{namenode_ip}}:9000/input hdfs://{{namenode_ip}}:9000/output
