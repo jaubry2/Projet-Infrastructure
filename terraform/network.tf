55be7ea5e01f81c2865c03638937d243a3a0d0d1
jojojn02 <joshuajn02@gmail.com>
2025-12-24 00:53:04 +0000

    VPC cluster-spark (subnet,firewall,nat,default_route)

---
+++ terraform/network.tf
@@ -0,0 +1,106 @@
+terraform { 
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cluster-spark-482013"
+  region  = "us-central1"
+
+}
+
+# VPC (MTU 1460)
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-cluster-spark"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+# Subnet IPv4 only: 10.10.10.0/24
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-cluster-spark"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  ip_cidr_range = "10.10.10.0/24"
+
+  # IPv4 only
+  stack_type = "IPV4_ONLY"
+
+  # Souvent utile si tes VM privées doivent accéder aux APIs Google via IP privées
+  private_ip_google_access = true
+} 
+
+
+# Cloud Router (requis pour Cloud NAT)
+resource "google_compute_router" "router" {
+  name    = "router-cluster-spark"
+  region  = "us-central1"
+  network = google_compute_network.vpc_network.id
+}
+
+# Cloud NAT (IPv4 only)
+resource "google_compute_router_nat" "nat" {
+  name   = "nat-cluster-spark"
+  router = google_compute_router.router.id
+  region = "us-central1"
+
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  # Logs optionnels (pratique pour debug)
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+# Ingress depuis Internet : ICMP + SSH
+resource "google_compute_firewall" "allow_icmp_ssh" {
+  name      = "fw-allow-icmp-ssh-from-internet"
+  network   = google_compute_network.vpc_network.id
+  direction = "INGRESS"
+  priority  = 1000
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+# Ingress depuis le subnet : tout autoriser
+resource "google_compute_firewall" "allow_all_from_subnet" {
+  name      = "fw-allow-all-from-subnet"
+  network   = google_compute_network.vpc_network.id
+  direction = "INGRESS"
+  priority  = 900  # plus prioritaire que 1000 (plus petit = plus prioritaire)
+
+  source_ranges = [google_compute_subnetwork.subnet.ip_cidr_range]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+resource "google_compute_route" "default_internet" {
+  name       = "default-internet-route"
+  network    = google_compute_network.vpc_network.id
+  dest_range = "0.0.0.0/0"
+  priority   = 1000
+
+  next_hop_gateway = "default-internet-gateway"
+}
