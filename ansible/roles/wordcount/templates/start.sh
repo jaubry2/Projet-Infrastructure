039adc2594652d6aa0541e90fcbaafb8519ea7df
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-12-29 13:08:47 +0100

    fix : modification du script pour exporter spark et hadoop lors de l'exec de la commande en ssh

--- ansible/roles/wordcount/templates/start.sh
+++ ansible/roles/wordcount/templates/start.sh
@@ -1,3 +1,6 @@
+export HADOOP_HOME=/opt/hadoop/hadoop-2.7.1
+export SPARK_HOME=/opt/spark/spark-2.4.3-bin-hadoop2.7
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SPARK_HOME/bin:$SPARK_HOME/sbin
 
 rm -rf /tmp/hadoop*
 ssh {{datanodes_ips.split(",")[0]}} rm -rf /tmp/hadoop*
