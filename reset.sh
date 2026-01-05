8fc15ed90cd1b2287ded3739ed860def15f86606
jojojn02 <joshuajn02@gmail.com>
2026-01-05 02:08:41 +0000

    ajouts inventory.ini dans les dossiers ansible

--- reset.sh
+++ reset.sh
@@ -2,4 +2,4 @@ cd terraform_edge/ && terraform destroy -auto-approve \
 && cd ../terraform_bigdata_cluster && terraform destroy -auto-approve \
 && terraform apply -auto-approve \
 && cd ../terraform_edge && terraform apply -auto-approve \
-&& cd .. && sleep 65 && echo "******petite pause de 65s avant d'entamer les playbook ansibles, ne pas quitter.******" && ./ansible.sh
+&& cd .. && echo "******petite pause de 65s avant d'entamer les playbook ansibles, ne pas quitter.******" && sleep 65 && ./ansible.sh
