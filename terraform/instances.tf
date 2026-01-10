e3c3faa8a21a59716c826479a9b8b83afcf7d890
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2026-01-10 12:35:12 +0100

    refacto : on retire l'ip publique des workers

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "nodes" {
     dynamic "access_config" {
       # La condition pour attribuer une IP publique (le bloc 'access_config')
       # est vraie si 'each.key' est 'master', 'edge', ou 'worker-1'.
-      for_each = (each.key == "master" || each.key == "edge" || each.key == "worker-1") ? [1] : []
+      for_each = (each.key == "master" || each.key == "edge") ? [1] : []
 
       content {
         # Si vous voulez l'IP temporaire par défaut, le bloc 'content' reste vide.
